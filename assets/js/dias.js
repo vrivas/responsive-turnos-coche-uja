c6291b2e0ce1561f693f015e2330cf0cc577bd93
Victor M. Rivas <vrivas@ujaen.es>
2024-09-25 18:15:45 +0200

    Sigo configurando cómo se muestran los datos

--- assets/js/dias.js
+++ assets/js/dias.js
@@ -10,7 +10,7 @@ function diaToDiv( ) {
     if( this.fecha.toComparableString()==(new Date()).toComparableString() ) idDia="id='hoy'";
     clasesDia=clasesDia.join(" ");
     // Contenido de cada dia
-    let divFecha=`<div class='fecha-dia'>${this.fecha.toDD_MMM()}</div>`;
+    let divFecha=`<div class='fecha-dia'>${this.fecha.toDW_DD_MMM()}</div>`;
     let divEficiencia=""//`<div class='eficiencia-dia'>Eficiencia ${this.eficiencia}</div>`;
     let divTurnos="";
     this.infoTurnos.filter( it => !null).forEach(it => divTurnos+=infoTurnoToDiv(it) );
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,4 +1,5 @@
 const NOMBRE_MESES_3_LETRAS = ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"]; 
+const NOMBRE_DIAS_3_LETRAS = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
 function fecha( dia, mes, anio) {
     return new Date(anio, mes-1, dia)
 }
@@ -23,9 +24,14 @@ Date.prototype.toDD_MMM_YYY = function() {
     let day=this.getDate();
     return day+"-"+NOMBRE_MESES_3_LETRAS[month]+"-"+year;
 }
+
 Date.prototype.toDD_MMM = function() {
     let tmp=this.toDD_MMM_YYY();
     return tmp.substring(0,tmp.length-5);
+}
+Date.prototype.toDW_DD_MMM = function() {
+    let tmp=this.toDD_MMM_YYY();
+    return NOMBRE_DIAS_3_LETRAS[ this.getDay()]+", "+tmp.substring(0,tmp.length-5);
 }
 
 function comparaFechas( fecha1, fecha2 ) {
